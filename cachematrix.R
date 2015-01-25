dbc5e1cf78caee0ccd2a314863eb1e3eadf58e7e
dadiboyena <dadiboyena@gmail.com>
2015-01-25 14:04:27 -0500

    Edited with my functions (also include description)

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,11 @@
-## Matrix inversion has the advantage to cache the inverse of a matrix in order to negate
+## Matrix inversion has the advantage to cache the inverse of a matrix in order to avoid 
 ## the repeated computation. The following functions computes to cache the inverse of a matrix.
 
-## makeCacheMatrix creates a list containing a function to
-## a. set the matrix value
-## b. get the matrix value
-## c. set the value of "inverse" of the matrix
-## d. get the value of "inverse" of the matrix
-
-
+    ## makeCacheMatrix creates a list containing a function to
+            ## a. set the matrix value
+            ## b. get the matrix value
+            ## c. set the value of "inverse" of the matrix
+            ## d. get the value of "inverse" of the matrix
 
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -26,9 +24,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## The following function returns the inverse of the matrix. It first checks if
-## the inverse has already been calculated. If so, it gets the result "GETTING CACHED DATA" 
-## and skips the calculation. If not, it calculated the inverse, sets the value in the cache via
+## The following function returns the inverse of the matrix. Firstly, It checks if
+## the inverse has already been calculated. If YES, it gets the result "GETTING CACHED DATA" 
+## and skips the calculation. If NOT, it calculated the inverse, sets the value in the cache via
 ## setmatrix function.
 
 cacheSolve <- function(x=matrix(), ...) {
@@ -44,21 +42,22 @@ cacheSolve <- function(x=matrix(), ...) {
 }
 
 ## An Example of the run is provided below for convenience
-## x = rbind(c(1, -1/3), c(-1/3, 1))
-## m = makeCacheMatrix(x)
-## m$get()
-## [,1]       [,2]
-## [1,]  1.0000000 -0.3333333
-## [2,] -0.3333333  1.0000000
+        ## x = rbind(c(1, -1/3), c(-1/3, 1))
+        ## m = makeCacheMatrix(x)
+            ## m$get()
+            ## [,1]       [,2]
+            ## [1,]  1.0000000 -0.3333333
+            ## [2,] -0.3333333  1.0000000
 
-## "first run"
-## cacheSolve(m)
-## [,1]  [,2]
-## [1,] 1.125 0.375
-## [2,] 0.375 1.125
+        
+        ## "first run"
+            ## cacheSolve(m)
+            ## [,1]  [,2]
+            ## [1,] 1.125 0.375
+            ## [2,] 0.375 1.125
 
-## retrieving the cache (second run)
-## GETTING CACHED DATA
-## [,1]  [,2]
-## [1,] 1.125 0.375
-## [2,] 0.375 1.125
+        ## retrieving the cache (second run)
+            ## GETTING CACHED DATA
+            ## [,1]  [,2]
+            ## [1,] 1.125 0.375
+            ## [2,] 0.375 1.125
