1e8a771f0125c3576a267e2bbe36aa19e925e751
dadiboyena <dadiboyena@gmail.com>
2015-01-25 13:54:04 -0500

    Edited with written functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
-## Matrix inversion has the benefit to cache the inverse of a matrix thereby the negating
-## the repeated computation.
-## The following functions computes to cache the inverse of a matrix.
+## Matrix inversion has the advantage to cache the inverse of a matrix in order to negate
+## the repeated computation. The following functions computes to cache the inverse of a matrix.
+
 ## makeCacheMatrix creates a list containing a function to
 ## a. set the matrix value
 ## b. get the matrix value
@@ -11,11 +11,12 @@
 
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <-NULL
+    m <- NULL
     set <-function(y){
-        x <<-y
-        m <<-NULL
+        x <<- y
+        m <<- NULL
     }
+    
     get <-function() x
     setmatrix <-function(solve) m<<- solve
     getmatrix <-function() m
@@ -26,12 +27,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## The following function returns the inverse of the matrix. It first checks if
-## the inverse has already been calculated. If so, it gets the result "GETTING cACHED DATA" 
+## the inverse has already been calculated. If so, it gets the result "GETTING CACHED DATA" 
 ## and skips the calculation. If not, it calculated the inverse, sets the value in the cache via
-## setmatrix function.Write a short comment describing this function
+## setmatrix function.
 
 cacheSolve <- function(x=matrix(), ...) {
-    m <-x$getmatrix()
+    m <- x$getmatrix()
     if(!is.null(m)){
         message("GETTING CACHED DATA")
         return(m)
@@ -42,7 +43,7 @@ cacheSolve <- function(x=matrix(), ...) {
     m
 }
 
-## Example 
+## An Example of the run is provided below for convenience
 ## x = rbind(c(1, -1/3), c(-1/3, 1))
 ## m = makeCacheMatrix(x)
 ## m$get()
@@ -50,13 +51,13 @@ cacheSolve <- function(x=matrix(), ...) {
 ## [1,]  1.0000000 -0.3333333
 ## [2,] -0.3333333  1.0000000
 
-## No cache observed in the first run
+## "first run"
 ## cacheSolve(m)
 ## [,1]  [,2]
 ## [1,] 1.125 0.375
 ## [2,] 0.375 1.125
 
-## retrieving the cache in the second run
+## retrieving the cache (second run)
 ## GETTING CACHED DATA
 ## [,1]  [,2]
 ## [1,] 1.125 0.375
